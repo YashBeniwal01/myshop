9f5afdc8210b15b20294161cd52ba260d04668f7
YashBeniwal01 <beniwalyash35@gmail.com>
2024-03-02 10:12:36 +0530

    new

--- src/components/Box.js
+++ src/components/Box.js
@@ -13,33 +13,83 @@ function Box() {
         <div className = "product-image image1">
                     <div className="image-text">
                         <h4>Most Wanted</h4>
-                        <p>7 versions</p>
+                        <p>7999 Rs.</p>
                     </div>
         </div>
+        <div className = "product-image imagex">
+                    <div className="image-text">
+                        <h4>Sammer</h4>
+                        <p>6999 Rs.</p>
+                    </div>
+        </div>
+        <div className = "product-image imagey">
+                    <div className="image-text">
+                        <h4>Sammer pro</h4>
+                        <p>7499 Rs.</p>
+                    </div>
+        </div>
+        <div className = "product-image imagez">
+                    <div className="image-text">
+                        <h4>Air Snow</h4>
+                        <p>6599 Rs.</p>
+                    </div>
+        </div>
+
+        <div className = "product-image imagey">
+                    <div className="image-text">
+                        <h4>Sammer pro</h4>
+                        <p>7499 Rs.</p>
+                    </div>
+        </div>
+        <div className = "product-image imagez">
+                    <div className="image-text">
+                        <h4>Air Snow</h4>
+                        <p>6599 Rs.</p>
+                    </div>
+        </div>
+
         <div className = "product-image image1">
                     <div className="image-text">
                         <h4>Most Wanted</h4>
-                        <p>7 versions</p>
+                        <p>7999 Rs.</p>
                     </div>
         </div>
-        <div className = "product-image image1">
+        <div className = "product-image imagex">
                     <div className="image-text">
-                        <h4> Most Wanted </h4>
-                        <p>7 versions</p>
+                        <h4>Sammer</h4>
+                        <p>6999 Rs.</p>
                     </div>
         </div>
-        <div className = "product-image image1">
+
+        <div className = "product-image imagez">
                     <div className="image-text">
-                        <h4>Most Wanted</h4>
-                        <p>7 versions</p>
+                        <h4>Air Snow</h4>
+                        <p>6599 Rs.</p>
                     </div>
         </div>
-        <div className = "product-image image1">
+
+        <div className = "product-image imagey">
                     <div className="image-text">
-                        <h4>Most Wanted</h4>
-                        <p>7 versions</p>
+                        <h4>Sammer pro</h4>
+                        <p>7499 Rs.</p>
                     </div>
         </div>
+
+        <div className = "product-image imagey">
+                    <div className="image-text">
+                        <h4>Sammer pro</h4>
+                        <p>7499 Rs.</p>
+                    </div>
+        </div>
+
+        <div className = "product-image imagex">
+                    <div className="image-text">
+                        <h4>Sammer</h4>
+                        <p>6999 Rs.</p>
+                    </div>
+        </div>
+       
+
       
     </div>
     </div>
